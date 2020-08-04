a6685dd33b47a34175bbdc5dccb8d6f63be5991f
Randy Pantinople <randypantinople@yahoo.com>
2020-08-04 15:32:16 -0400

    first commit

---
+++ global.R
@@ -0,0 +1,3 @@
+library(shinydashboard)
+starbucks2 = read.csv('./data/starbucks2.csv')
+starbucks2
---
+++ ui.R
@@ -0,0 +1,7 @@
+library(shinydashboard)
+
+shinyUI(dashboardPage(
+  dashboardHeader(title='Starbucks Global Presence'),
+  dashboardSidebar('Randy Pantinople'),
+  dashboardBody(),
+))
